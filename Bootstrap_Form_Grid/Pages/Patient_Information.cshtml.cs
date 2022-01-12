a19dd73b1ab1c01a27e1b7d6d259968133be7692
Creg Sullivan <cregcsullivan1024@gmail.com>
2022-01-12 00:12:34 -0600

    Add project files.

---
+++ Bootstrap_Form_Grid/Pages/Patient_Information.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Bootstrap_Form_Grid.Pages
+{
+    public class Patient_InformationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
